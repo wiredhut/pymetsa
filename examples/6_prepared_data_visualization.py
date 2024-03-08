75f9180887d1e1bc5951f70af6d7b06003d4123f
Dreamlone <mik_sar@mail.ru>
2024-03-08 10:31:49 +0200

    add template

---
+++ examples/6_prepared_data_visualization.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import numpy as np
+import rasterio
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from pymetsa.paths import get_data_folder_path
+
+
+def visualize_features_and_target_dependencies():
+    """ Algorithm allows to show matrices """
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    visualize_features_and_target_dependencies()
